24deec5f259a0e3987229f504587c5735afa03a8
Antonov, Sergey <sergey.antonov@nedra.digital>
2020-12-09 12:43:21 +0300

    start app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React, { lazy, Suspense } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
-const Map = lazy(() => import('./layouts/Map/Map'));
+const Tickets = lazy(() => import('./layouts/Tickets/Tickets'));
 
 const App = () => (
   <Suspense fallback={<LinearProgress variant="query" />}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Map} />
+        <Route exact path="/" component={Tickets} />
       </Switch>
     </BrowserRouter>
   </Suspense>
